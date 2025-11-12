a261b7edce4f33d1dccb1efa587cd987e6d70518
Justus <justus.d2025@gmail.com>
2025-11-12 15:24:40 +0100

    fixed ffmpeg argument errors

--- src/ffmpeg_audio.bat
+++ src/ffmpeg_audio.bat
@@ -2,6 +2,12 @@ set "CHANNELS=2"
 set "BITRATE=128k"
 set "SAMPLERATE=44100" 
 set "ENCODER_FLAGS=-codec:a libmp3lame -b:a %BITRATE% -ar %SAMPLERATE% -ac %CHANNELS%" 
+echo %ENCODER_FLAGS%
 
-call ./src/ffmpeg_call.bat "%1" "%ENCODER_FLAGS%" "%2"
+echo Started converting %1 to %2
+if NOT "%3"=="" (
+    echo ENCODER_FLAGS: %3
+)
+
+ffmpeg -i %1 %ENCODER_FLAGS% -y %2
 pause 2>NUL
