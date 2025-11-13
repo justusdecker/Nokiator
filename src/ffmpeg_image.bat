35b27ddcfb0158c546776d26f381fa277d8305d0
Justus <justus.d2025@gmail.com>
2025-11-14 00:16:09 +0100

    make ffmpeg silent

--- src/ffmpeg_image.bat
+++ src/ffmpeg_image.bat
@@ -2,5 +2,5 @@ set "RESOLUTION=640:480"
 set "FORMAT=rgb24"
 set "DPI=96"
 set "QUALITY=20"
-set "ENCODER_FLAGS=-vf "scale=%RESOLUTION%,format=%FORMAT%" -q:v %QUALITY% -dpi %DPI%" 
+set "ENCODER_FLAGS=-hide_banner -loglevel error -vf "scale=%RESOLUTION%,format=%FORMAT%" -q:v %QUALITY% -dpi %DPI%" 
 ffmpeg -i %1 %ENCODER_FLAGS% %2
