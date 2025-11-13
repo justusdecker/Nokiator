d1f48362edb19fa3bc66af7a6a0b003e753dc702
Justus <justus.d2025@gmail.com>
2025-11-13 23:55:59 +0100

    fixed output path

--- src/ffmpeg_image.bat
+++ src/ffmpeg_image.bat
@@ -3,4 +3,4 @@ set "FORMAT=rgb24"
 set "DPI=96"
 set "QUALITY=20"
 set "ENCODER_FLAGS=-vf "scale=%RESOLUTION%,format=%FORMAT%" -q:v %QUALITY% -dpi %DPI%" 
-ffmpeg -i %1 %ENCODER_FLAGS% output.jpg
+ffmpeg -i %1 %ENCODER_FLAGS% %2
